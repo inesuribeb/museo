cbf353d4883ed304528d94936579a9d00ef819ef
inesuribeb <inesuribeb@gmail.com>
2026-03-26 18:26:28 +0100

    miembros y colaboradores

--- mi-aplicacion/src/pages/Foundation/sections/Team/collaborators/Collaborators.jsx
+++ mi-aplicacion/src/pages/Foundation/sections/Team/collaborators/Collaborators.jsx
@@ -88,7 +88,91 @@ const CollaboratorsMembers = [
         },
     },
     {
-        id: 4,
+        id: 5,
+        img: "/Images/Team/Nathalie.jpeg",
+        name: "Nombre",
+        surname: "Apellido",
+        role: {
+            es: "Jefe de planta",
+            en: "Jefe de planta",
+            pt: "Jefe de planta",
+        },
+        quote: {
+            es: "Que bonita es la vida",
+            en: "Que bonita es la vida",
+            pt: "Que bonita es la vida",
+        },
+        bio: {
+            es: "Estudía farmacia y churrería",
+            en: "Estudía farmacia y churrería",
+            pt: "Estudía farmacia y churrería",
+        },
+    },
+    {
+        id: 6,
+        img: "/Images/Team/Nathalie.jpeg",
+        name: "Nombre",
+        surname: "Apellido",
+        role: {
+            es: "Jefe de planta",
+            en: "Jefe de planta",
+            pt: "Jefe de planta",
+        },
+        quote: {
+            es: "Que bonita es la vida",
+            en: "Que bonita es la vida",
+            pt: "Que bonita es la vida",
+        },
+        bio: {
+            es: "Estudía farmacia y churrería",
+            en: "Estudía farmacia y churrería",
+            pt: "Estudía farmacia y churrería",
+        },
+    },
+    {
+        id: 7,
+        img: "/Images/Team/Nathalie.jpeg",
+        name: "Nombre",
+        surname: "Apellido",
+        role: {
+            es: "Jefe de planta",
+            en: "Jefe de planta",
+            pt: "Jefe de planta",
+        },
+        quote: {
+            es: "Que bonita es la vida",
+            en: "Que bonita es la vida",
+            pt: "Que bonita es la vida",
+        },
+        bio: {
+            es: "Estudía farmacia y churrería",
+            en: "Estudía farmacia y churrería",
+            pt: "Estudía farmacia y churrería",
+        },
+    },
+    {
+        id: 8,
+        img: "/Images/Team/Nathalie.jpeg",
+        name: "Nombre",
+        surname: "Apellido",
+        role: {
+            es: "Jefe de planta",
+            en: "Jefe de planta",
+            pt: "Jefe de planta",
+        },
+        quote: {
+            es: "Que bonita es la vida",
+            en: "Que bonita es la vida",
+            pt: "Que bonita es la vida",
+        },
+        bio: {
+            es: "Estudía farmacia y churrería",
+            en: "Estudía farmacia y churrería",
+            pt: "Estudía farmacia y churrería",
+        },
+    },
+    {
+        id: 9,
         img: "/Images/Team/Nathalie.jpeg",
         name: "Nombre",
         surname: "Apellido",
@@ -111,11 +195,32 @@ const CollaboratorsMembers = [
 
 ]
 
+
+// function Collaborators({ t }) {
+//     return (
+//         <div className='collab-section-intern'>
+//             <div className='collab-grid'>
+//                 <h1 className='collab-grid-title'>Colaboradores</h1>
+//                 {CollaboratorsMembers.map((member, index) => (
+//                     <CollabCard key={member.id} member={member} t={t} />
+//                 ))}
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default Collaborators;
+
 function Collaborators({ t }) {
     return (
         <div className='collab-section-intern'>
-            <h1>Collaborators</h1>
             <div className='collab-grid'>
+                <div className='collab-title-placeholder'>
+                    {/* <h1 className='collab-grid-title'>Colaboradores</h1> */}
+                    <div className='collab-title-wrapper'>
+                        <h1 className='collab-grid-title'>Colaboradores</h1>
+                    </div>
+                </div>
                 {CollaboratorsMembers.map((member) => (
                     <CollabCard key={member.id} member={member} t={t} />
                 ))}
