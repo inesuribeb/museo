432c03036fc6dde44fc237b5edb358b6612179bc
inesuribeb <inesuribeb@gmail.com>
2026-09-01 13:13:30 +0200

    contenido featured

--- mi-aplicacion/src/pages/Home/sections/Featured/Featured.jsx
+++ mi-aplicacion/src/pages/Home/sections/Featured/Featured.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import { useLocalizedData } from '../../../../components/Hooks/Hooks';
 import GenericCard from '../../../../components/GenericCard/GenericCard';
 import './Featured.css';
@@ -5,53 +6,62 @@ import './Featured.css';
 const FeaturedItems = [
     {
         id: 1,
-        img: '',
-        tab: { es: "Exposiciones", en: "Exposiciones", pt: "Exposiciones" },
-        title: { es: "Expo Residencia2025", en: "Expo Residencia2025", pt: "Expo Residencia2025" },
-        description: { es: "Muestra de la Residencia2025 en Madrid", en: "Muestra de la Residencia2025 en Madrid", pt: "Muestra de la Residencia2025 en Madrid" }
+        img: '/Images/Residencies/Residency01/PortadaResi.jpg',
+        tab: { es: "Residencias", en: "Residencies", pt: "Residências" },
+        title: { es: "Residencia2025", en: "2025Residency", pt: "Residência2025" },
+        description: { es: 'Escritura, Fotografía, y Cocina', en: 'Writing, Photography, and Cooking', pt: 'Escrita, Fotografia, e Culinária' },
+        link: '/residencia/residencia-1',
     },
     {
         id: 2,
-        img: '',
-        tab: { es: "", en: "", pt: "" },
-        title: { es: "", en: "", pt: "" },
-        description: { es: "", en: "", pt: "" }
+        img: '/Images/Exhibitions/Exhibition01/Cover-expo1.jpg',
+        tab: { es: "Exposiciones", en: "Exhibitions", pt: "Exposiçãos" },
+        title: { es: "Expo Residencia2025", en: "Residency2025 Expo", pt: "Expo Residência2025" },
+        description: { es: "Muestra de la Residencia2025 en Madrid", en: "Sample of Residencia2025 in Madrid", pt: "Exposição da Residência2025 em Madrid" },
+        link: '/exposicion/expo-1',
     },
     {
         id: 3,
-        img: '',
-        tab: { es: "", en: "", pt: "" },
-        title: { es: "", en: "", pt: "" },
-        description: { es: "", en: "", pt: "" }
+        img: '/Images/Publications/Publication01/Publicaciones-1.jpg',
+        tab: { es: "Publicaciones", en: "Publication", pt: "Publicaçãos" },
+        title: { es: "Una residencia narrada por: Ivan Floro", en: "A Residency Narrated by: Ivan Floro", pt: "Uma residência narrada por: Ivan Floro" },
+        description: { es: "Primera edición de la serie editorial <em>Una residencia narrada por</em>", en: "First edition of the editorial series <em>A residency narrated by</em>", pt: "Primeira edição da série editorial <em>Uma residência narrada por</em>" },
+        link: '/publicacion/pub-1',
     },
     {
         id: 4,
-        img: '',
-        tab: { es: "", en: "", pt: "" },
-        title: { es: "", en: "", pt: "" },
-        description: { es: "", en: "", pt: "" }
+        img: '/Images/Home/Intro/Intro2-obras.jpg',
+        tab: { es: "Obras colectivas", en: "Collective works", pt: "Obras coletivas" },
+        title: { es: "Memoria sense mancha", en: "Memoria sense mancha", pt: "Memoria sense mancha" },
+        description: { es: "Instalación • Finca El Azahar", en: "Installation • Finca El Azahar", pt: "Instalação • Finca El Azahar" },
+        link: '/obra-colectiva/artPiece-1',
     },
     {
         id: 5,
-        img: '',
-        tab: { es: "", en: "", pt: "" },
-        title: { es: "", en: "", pt: "" },
-        description: { es: "", en: "", pt: "" }
+        img: '/Images/Exhibitions/Exhibition02/libro1bien.webp',
+        tab: { es: "Exposiciones", en: "Exhibitions", pt: "Exposiçãos" },
+        title: { es: "Presentación <em>Una residencia narrada por Ivan Floro</em>", en: "", pt: "" },
+        description: { es: "Presentación en Barcelona del libro resultado de la Residencia2025", en: "", pt: "" },
+        link: '/exposicion/expo-2',
     },
     {
         id: 6,
-        img: '',
-        tab: { es: "", en: "", pt: "" },
-        title: { es: "", en: "", pt: "" },
-        description: { es: "", en: "", pt: "" }
+        img: '/Images/Collaborations/Collab01/Collab01-4.jpg',
+        tab: { es: "Colaboraciones", en: "Collaborations", pt: "Colaboraçãos" },
+        title: { es: "<em>Blanco</em>, una colaboración con el Circulo de Bellas Artes", en: "em>Blanco</em>, a collaboration with Círculo de Bellas Artes", pt: "<em>Blanco</em>, uma colaboração com o Círculo de Belas Artes" },
+        description: { es: "", en: "", pt: "" },
+        link: '/colaboracion/collab-1',
     }
 ];
 
 function Featured({ t, getRoute }) {
+    const navigate = useNavigate();
     const localizedItems = useLocalizedData(FeaturedItems);
 
     const handleClick = (item) => {
-        // navega con getRoute, ej: navigate(getRoute('archive', { id: item.id }))
+        if (item.link) {
+            navigate(item.link);
+        }
     };
 
     return (
